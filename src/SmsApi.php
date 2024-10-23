455c243e55d62f78cd4306e74eba33602cb34219
Kazi Rayhan Reza Tirtho <83582861+Kazi-Rayhan@users.noreply.github.com>
2024-10-23 13:49:12 +0600

    Update SmsApi.php

--- src/SmsApi.php
+++ src/SmsApi.php
@@ -4,17 +4,17 @@ namespace Vemcogroup\SmsApi;
 
 use Smsapi\Client\Curl\SmsapiHttpClient;
 use Smsapi\Client\Feature\Sms\Bag\SendSmsBag;
+use Smsapi\Client\Feature\Sms\Data\Sms;
 use Vemcogroup\SmsApi\Exceptions\SmsApiException;
 
 class SmsApi
 {
-    public static function send($to, $from, $message): Sms
+    public static function send($to,$message): Sms
     {
         try {
             $apiToken = config('smsapi.token');
 
             $sms = SendSmsBag::withMessage($to, $message);
-            $sms->from = $from;
             $sms->normalize = true;
 
             $service = (new SmsapiHttpClient())->smsapiComService($apiToken);
